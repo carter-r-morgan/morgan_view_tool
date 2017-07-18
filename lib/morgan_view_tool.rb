f8440482d26f65cc688591f7d8bd61814fd02686
slcwestdevcamp <slcwest@devcamp.com>
2017-07-18 17:31:22 +0000

    Dev tool initial commit

---
+++ lib/morgan_view_tool.rb
@@ -0,0 +1,6 @@
+require "morgan_view_tool/version"
+require "morgan_view_tool/renderer"
+
+module MorganViewTool
+  # Your code goes here...
+end
---
+++ spec/morgan_view_tool_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe MorganViewTool do
+  it "has a version number" do
+    expect(MorganViewTool::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
